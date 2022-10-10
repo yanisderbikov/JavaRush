f4d23c12963efcaefe8613d65449be9dd4edce1f
yanderbikovmail.ru <yanisderbikov@gmail.com>
2022-10-10 11:42:23 +0300

    done1915

---
+++ src/task1915/Solution.java
@@ -0,0 +1,19 @@
+package task1915;
+
+/*
+Дублируем текст
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
+
