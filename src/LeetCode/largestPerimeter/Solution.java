24de1861d897147459a7a4fe767cff8d0bbb5e4d
yanderbikovmail.ru <yanisderbikov@gmail.com>
2022-10-12 07:30:39 +0300

    LeetCode: largestPerimeter

--- src/LeetCode/largestPerimeter/Solution.java
+++ src/LeetCode/largestPerimeter/Solution.java
@@ -6,11 +6,12 @@ public class Solution {
     public int largestPerimeter(int[] nums) {
         Arrays.sort(nums);
         int length = nums.length;
-        if (!(nums[length - 1] > nums[length - 2] - nums[length - 3])){
-            return 0;
-        } else {
-            int sum = nums[length - 1] + nums[length - 2] + nums[length - 3];
-            return sum;
+        while (length >= 3) {
+            if (nums[length - 1] < nums[length - 2] + nums[length - 3]) {
+                return nums[length - 1] + nums[length - 2] + nums[length - 3];
+            }
+            length--;
         }
+        return 0;
     }
 }
