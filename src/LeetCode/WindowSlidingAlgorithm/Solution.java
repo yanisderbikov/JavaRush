1edc0cc523a5b7ddecde94b47fb31ed719fc6f21
yanderbikovmail.ru <yanisderbikov@gmail.com>
2022-10-13 21:22:24 +0300

    LeetCode: window sliding algorithm

---
+++ src/LeetCode/WindowSlidingAlgorithm/Solution.java
@@ -0,0 +1,26 @@
+package LeetCode.WindowSlidingAlgorithm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+
+    //    Window Sliding Algorithm
+
+    public int lengthOfLongestSubstring(String s) {
+
+        Map<Character, Integer> visitedCharacters = new HashMap<>();
+        int maxLenght = 0;
+        for (int right = 0, left = 0; right < s.length(); right++){
+            char currentChar = s.charAt(right);
+            if (visitedCharacters.containsKey(currentChar)
+                    && visitedCharacters.get(currentChar) >= left) {
+                left = visitedCharacters.get(currentChar) + 1;
+            }
+            visitedCharacters.put(currentChar, right);
+            maxLenght = Math.max(maxLenght, right - left + 1 );
+
+        }
+        return maxLenght;
+    }
+}
