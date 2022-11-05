b1783289449e5d7037dd80b7f02e43f18f43a173
yanderbikovmail.ru <yanisderbikov@gmail.com>
2022-11-05 17:08:13 +0300

    _7_ reverse Integer - FastSolution - done

--- src/LeetCode/_7_ReverseInteger/FastSolution.java
+++ src/LeetCode/_7_ReverseInteger/FastSolution.java
@@ -1,21 +1,24 @@
 package LeetCode._7_ReverseInteger;
 
+
+//  1534236469
+
 public class FastSolution {
     public int reverse(int x) {
-        int result = 0;
+        long result = 0;
+        int maxValue = Integer.MAX_VALUE;
         if (x == 0){
             return 0;
         }
 
         while (x != 0) {
-            result = result*10 + x % 10;
+            result = result * 10 + x % 10;
             x /= 10;
-            if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE){
+            if (result > maxValue || result < -maxValue){
                 return 0;
             }
         }
-        return result;
-        
+        return (int) result;
     }
 }
 
--- src/LeetCode/_7_ReverseInteger/Main.java
+++ src/LeetCode/_7_ReverseInteger/Main.java
@@ -4,7 +4,8 @@ import java.io.FilterOutputStream;
 
 public class Main {
     public static void main(String[] args) {
-        Solution solution = new Solution();
+//        Solution solution = new Solution();
+        FastSolution solution = new FastSolution();
         int result = solution.reverse(1534236469);
         System.out.println(result);
 
