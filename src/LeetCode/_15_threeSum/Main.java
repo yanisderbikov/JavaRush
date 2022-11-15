00967cf93be14a2537b5b103c7aaa0e530b34628
yanderbikovmail.ru <yanisderbikov@gmail.com>
2022-11-15 06:56:26 +0300

    _15_ I was too close. There should be a solution

---
+++ src/LeetCode/_15_threeSum/Main.java
@@ -0,0 +1,10 @@
+package LeetCode._15_threeSum;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.threeSum(new int[]{-1,0,1,2,-1,-4});
+
+    }
+
+}
---
+++ src/LeetCode/_15_threeSum/Solution.java
@@ -0,0 +1,38 @@
+package LeetCode._15_threeSum;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+//        итерируемся по первому элементу
+//        в этом форе мы ищем пару, которая будет давть target
+        List<List<Integer>> listOfLists = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int first = nums[i], indexFirst = i;
+            Map<Integer, Integer> map = new HashMap<>(); // K -> index; V -> sum to 0
+            for (int j = i+1; j < nums.length; j++) {
+                int second = nums[j], indexSecond = j;
+                int target = -(first + second); // 0
+                if (map.containsValue(target)){
+                    for (var pair : map.entrySet()){
+                        if (pair.getValue() == target){
+                            int indexThird = pair.getKey();
+                            List<Integer> list = new ArrayList<>(){{
+                                add(indexFirst);
+                                add(indexSecond);
+                                add(indexThird);
+                            }};
+                            listOfLists.add(list);
+                        }
+                    }
+                }else {
+                    map.put(j, target);
+                }
+            }
+        }
+        return listOfLists;
+    }
+}
