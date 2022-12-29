371b693fbdc84de944a8e6fb6bb0364bdc855696
yanderbikovmail.ru <yanisderbikov@gmail.com>
2022-12-29 21:46:08 +0500

    LeetCode algorithm | DAY -- (BackTraking LinkedList)

--- src/LeetCode/Main.java
+++ src/LeetCode/Main.java
@@ -1,10 +1,6 @@
 package LeetCode;
 import LeetCode._567_Permution_un_String.Solution;
 
-import java.security.KeyPair;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
 
 //LeetCode._567_Permution_un_String
 //import LeetCode._733_Flood_Fill.Solution;
@@ -12,18 +8,11 @@ public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.checkInclusion("abs", "hellobasrre");
-        char curren = 65;
-        int d = (int) 'a';
 
-        System.out.println(curren);
-        System.out.println(d);
-
-        Queue queue = new LinkedList<>();
-        Queue queue1 = new PriorityQueue<>();
+        solution.checkInclusion3("abcdxabcde", "abcdeabcdx");
 
 
-        
+
 
 
     }
--- src/LeetCode/_567_Permution_un_String/Solution.java
+++ src/LeetCode/_567_Permution_un_String/Solution.java
@@ -1,5 +1,7 @@
 package LeetCode._567_Permution_un_String;
 
+import java.util.HashMap;
+
 public class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int[] map = new int[26];
@@ -19,6 +21,78 @@ public class Solution {
                 count_chars++;
         }
         return false;
+    }
+    public boolean checkInclusion2(String s1, String s2) {
+        int[] ch_arr = new int[128];
+
+        for (int i = 0; i < s1.length(); i++) {
+            ch_arr[s1.charAt(i) ]++;
+        }
+
+        int left = 0;
+        int right = 0;
+        int count_chars = s1.length();
+
+        while (right < s2.length()){
+            if (ch_arr[s2.charAt(right++) ]-- > 0){
+                count_chars--;
+            }
+            if (count_chars == 0){
+                return true;
+            }
+            if (right - left == s1.length()
+                    && ch_arr[s2.charAt(left++) ]++ >= 0){
+                count_chars++;
+            }
+        }
+        return false;
+    }
+    public boolean checkInclusion3(String s1, String s2) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        int count = 0;
+
+        for (int i = 0; i < s1.length(); i++) {
+            char current_char = s1.charAt(i);
+            if (map.containsKey(current_char)){
+                map.put(current_char, map.get(current_char) + 1);
+            }else {
+                map.put(current_char, 1);
+            }
+            count++;
+        }
+
+        int left = 0, right = 0;
+
+        while (right < s2.length()){
+
+            char rightChar = s2.charAt(right);
+            char leftChar = s2.charAt(left);
+
+            if (map.containsKey(rightChar)){
+                map.put(rightChar, map.get(rightChar) - 1);
+            }
+            right++;
+
+            int res = count;
+            for (int n : map.values()){
+                if (n == 0){
+                    res++;
+                }else {
+                    break;
+                }
+            }
+            if (res == 0) return true;
+
+            if (right - left == s1.length()) {
+                if (map.containsKey(leftChar)){
+                    map.put(leftChar, map.get(leftChar) + 1);
+                }
+                left++;
+            }
+        }
+        return false;
+
     }
 }
 
