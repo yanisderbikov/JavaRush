3203c864fb244760158bcbecf3e8e5f51dc35caa
yanderbikovmail.ru <yanisderbikov@gmail.com>
2023-01-11 23:55:27 +0300

    LeetCode algorithm DAY 11 | 77

--- src/LeetCode/_77_Combinations/Solution.java
+++ src/LeetCode/_77_Combinations/Solution.java
@@ -6,12 +6,18 @@ import java.util.List;
 public class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> ans = new LinkedList();
-        if (n < k || k == 0) return ans;
+        if (n < k || k == 0) {
+            return ans;
+        }
 
         // Case I: Add number n to answer
         ans = combine(n-1, k-1);
-        if (ans.isEmpty()) ans.add(new LinkedList<Integer>()); // base case, initialize empty list
-        for (List<Integer> list:ans) list.add(n);              // add n to final ans
+        if (ans.isEmpty()) {
+            ans.add(new LinkedList<Integer>()); // base case, initialize empty list
+        }
+        for (List<Integer> list:ans) {
+            list.add(n);              // add n to final ans
+        }
 
         // Case II: Do not add number n to answer
         ans.addAll(combine(n-1, k));
