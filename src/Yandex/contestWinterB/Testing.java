2e2192c48cac18cfc3d561fbfb28e9e5263075ee
yanderbikovmail.ru <yanisderbikov@gmail.com>
2023-01-14 22:57:36 +0300

    LeetCode algorithm DAY 11 | 77

---
+++ src/Yandex/contestWinterB/Testing.java
@@ -0,0 +1,21 @@
+package Yandex.contestWinterB;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Objects;
+
+public class Testing {
+    static HashMap<Integer, HashMap<LocalDateTime, Character>> map = new HashMap<>();
+
+    public static void main(String[] args) {
+        String str1 = "50 7 25 3632 A";
+        String str2 = "14 23 52 212372 S";
+
+
+    }
+
+}
+
+
