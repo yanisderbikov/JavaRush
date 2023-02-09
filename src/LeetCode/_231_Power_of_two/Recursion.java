af0ff7bf54de13f4da09d8c02c75674bd02ab7d8
yanderbikovmail.ru <yanisderbikov@gmail.com>
2023-02-09 08:29:38 +0300

    LeetCode algorithm 238 simple recusion and while loop

--- src/LeetCode/_231_Power_of_two/Recursion.java
+++ src/LeetCode/_231_Power_of_two/Recursion.java
@@ -5,11 +5,10 @@ public class Recursion {
         if (n == 1) return true;
         if (n == 0) return false;
 
-        if (n % 2 != 0) {
-            return false;
-        } else {
+        if (n % 2 == 0) {
             return isPowerOfTwo(n / 2);
         }
+        return false;
     }
 
 }
